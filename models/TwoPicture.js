3252fd2cd8acaac144e69571a715ad4edd799c68
Osman <114867365+osmansam@users.noreply.github.com>
2023-06-01 21:20:44 -0400

    delete function done

--- models/TwoPicture.js
+++ models/TwoPicture.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 
-const PictureSchema = new mongoose.Schema({
-  img: { type: String },
-  header: { type: String },
-  paragraphs: { type: [String] },
-  buttons: { type: [] },
-});
-
 const TwoPictureSchema = new mongoose.Schema({
   page: {
     type: String,
@@ -17,23 +10,7 @@ const TwoPictureSchema = new mongoose.Schema({
   position: { type: Number, required: true },
   language: { type: String, required: true },
   twoPictureArray: {
-    type: [PictureSchema],
-    validate: [
-      function validateTwoPictureArrayLength(val) {
-        switch (this.componentType) {
-          case "TwoPictureContainer":
-            return val.length === 2;
-          case "IconExplainContainer":
-            return val.length === 3;
-          case "PictureAtRight":
-          case "PictureAtLeft":
-            return val.length === 1;
-          default:
-            return true;
-        }
-      },
-      "Invalid length for twoPictureArray",
-    ],
+    type: [],
   },
 });
 
--- routes/twoPictureRoutes.js
+++ routes/twoPictureRoutes.js
@@ -5,11 +5,13 @@ const {
   getPageTwoPictures,
   createTwoPictures,
   updateTwoPictures,
+  updateNewsContainer,
   deleteTwoPictures,
 } = require("../controllers/twoPictureController");
 
 router.route("/:page").get(getPageTwoPictures);
 router.route("/").post(createTwoPictures);
+router.route("/updateContainer/:id").patch(updateNewsContainer);
 router.route("/:id").patch(updateTwoPictures).delete(deleteTwoPictures);
 
 module.exports = router;
