b98b68631656417ff0c2a0f7a74afc2e6bcbdef9
Osman <114867365+osmansam@users.noreply.github.com>
2023-09-01 22:06:49 -0500

    container logic is done

--- controllers/containerControler.js
+++ controllers/containerControler.js
@@ -14,13 +14,85 @@ const createContainer = async (req, res) => {
   });
   res.status(StatusCodes.CREATED).json({ container });
 };
-
+//get all containers
 const getAllContainers = async (req, res) => {
   const containers = await Container.find({});
   res.status(StatusCodes.OK).json({ containers });
+};
+// get container by id
+const getContainerById = async (req, res) => {
+  const { id: containerId } = req.params;
+  const container = await Container.findOne({ _id: containerId }).populate(
+    "twoPictures"
+  );
+  if (!container) {
+    throw new CustomError.NotFoundError(
+      `No container with id : ${containerId}`
+    );
+  }
+  res.status(StatusCodes.OK).json({ container });
+};
+//update container
+const updateContainer = async (req, res) => {
+  const { id: containerId } = req.params;
+  const { twoPictures, name, type } = req.body;
+
+  // Find the container by its _id
+  const container = await Container.findOne({ _id: containerId });
+  if (!container) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ error: "Container not found" });
+  }
+
+  // Retrieve the twoPictures associated with this container
+  const twoPicturesToDelete = container.twoPictures;
+
+  // Delete all the twoPictures associated with this container from the database
+  await TwoPicture.deleteMany({ _id: { $in: twoPicturesToDelete } });
+  const createdTwoPictures = await TwoPicture.insertMany(twoPictures);
+
+  // Update the container with the new data, including the updated twoPictures
+  const updatedContainer = await Container.findOneAndUpdate(
+    { _id: containerId },
+    {
+      name,
+      type,
+      twoPictures: createdTwoPictures.map((twoPicture) => twoPicture._id),
+    },
+    { new: true, runValidators: true }
+  ).populate("twoPictures");
+  res.status(StatusCodes.OK).json({ container: updatedContainer });
+};
+//delete Container
+const deleteContainer = async (req, res) => {
+  const { id: containerId } = req.params;
+
+  // Find the container by its _id
+  const container = await Container.findOne({ _id: containerId });
+
+  if (!container) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ error: "Container not found" });
+  }
+
+  // Retrieve the twoPictures associated with this container
+  const twoPicturesToDelete = container.twoPictures;
+  console.log(twoPicturesToDelete);
+  // Delete all the twoPictures associated with this container from the database
+  await TwoPicture.deleteMany({ _id: { $in: twoPicturesToDelete } });
+
+  // Now that the twoPictures are deleted, delete the container
+  await Container.findByIdAndDelete(containerId);
+
+  res.status(StatusCodes.NO_CONTENT).send();
 };
 
 module.exports = {
   createContainer,
   getAllContainers,
+  getContainerById,
+  updateContainer,
+  deleteContainer,
 };
--- routes/containerRoutes.js
+++ routes/containerRoutes.js
@@ -4,8 +4,16 @@ const router = express.Router();
 const {
   createContainer,
   getAllContainers,
+  getContainerById,
+  updateContainer,
+  deleteContainer,
 } = require("../controllers/containerControler");
 
 router.route("/").post(createContainer).get(getAllContainers);
+router
+  .route("/:id")
+  .get(getContainerById)
+  .patch(updateContainer)
+  .delete(deleteContainer);
 
 module.exports = router;
