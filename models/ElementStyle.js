03603d42790b9264a85ed46d9b8bea93fe5f1975
Osman <114867365+osmansam@users.noreply.github.com>
2023-09-06 17:37:02 -0500

    styling adding

--- models/ElementStyle.js
+++ models/ElementStyle.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const ElementStyleSchema = new mongoose.Schema({
   color: { type: String },
-  "text-size": { type: String },
+  "font-weight": { type: String },
   backgroundColor: { type: String },
   padding: { type: String },
   "font-size": { type: String },
+  "font-family": { type: String },
 });
 
 module.exports = mongoose.model("ElementStyle", ElementStyleSchema);
