cc48b8cb91d596761d6a8ae4c2e74efa6f2e696c
Osman <114867365+osmansam@users.noreply.github.com>
2023-09-11 20:42:05 -0500

    resume box made css changes

--- models/ResumeBox.js
+++ models/ResumeBox.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
+const ElementStyle = require("./ElementStyle");
 
 const ResumeBoxSchema = new mongoose.Schema({
-  header: { type: String },
-  year1: { type: String },
-  year2: { type: String },
-  university: { type: String },
-  paragraph: { type: String },
+  header: {
+    content: { type: String },
+    style: ElementStyle.schema, // Use ElementStyle schema for style
+  },
+  year1: {
+    content: { type: String },
+    style: ElementStyle.schema, // Use ElementStyle schema for style
+  },
+  year2: {
+    content: { type: String },
+    style: ElementStyle.schema, // Use ElementStyle schema for style
+  },
+  university: {
+    content: { type: String },
+    style: ElementStyle.schema, // Use ElementStyle schema for style
+  },
+  paragraph: {
+    content: { type: String },
+    style: ElementStyle.schema, // Use ElementStyle schema for style
+  },
 });
 
 module.exports = mongoose.model("ResumeBox", ResumeBoxSchema);
