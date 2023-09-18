4667fc0c0e8ef0f281b429ba3262be82c901d7e1
Osman <114867365+osmansam@users.noreply.github.com>
2023-09-18 09:45:36 -0500

    image style done

--- models/TwoPicture.js
+++ models/TwoPicture.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const ElementStyle = require("./ElementStyle");
 const ComponentStyle = require("./ComponentStyle");
+
 const TwoPictureSchema = new mongoose.Schema({
   page: {
     type: String,
